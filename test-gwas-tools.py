c037a620910f2fbe13ae25deb87d87a6d58e54a7
Yifei Ding <dingyifeiair@gmail.com>
2023-05-26 21:39:04 -0700

    test: add test for read_geno
    Signed-off-by: Yifei Ding<yifeiding@protonmail.com>

--- test-gwas-tools.py
+++ test-gwas-tools.py
@@ -1,14 +1,15 @@
 import unittest
+
+import numpy
 import pandas as pd
-from gwas_tools import generate_qqplot
+from gwas_tools import *
 import numpy as np
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(False, False)  # add assertion here
+
+class TestPlots(unittest.TestCase):
     def test_generate_qqplot_uniform(self):
         data = pd.DataFrame({
-            'pvalues': np.random.uniform(0,1,1000)
+            'pvalues': np.random.uniform(0, 1, 1000)
         })
 
         # Call the generate_plot function
@@ -16,9 +17,10 @@ class MyTestCase(unittest.TestCase):
 
         # Assert that no errors occurred during the plot generation
         self.assertTrue(True)
+
     def test_generate_qqplot_exponential(self):
         data = pd.DataFrame({
-            'pvalues': np.random.exponential(scale=1,size=1000)
+            'pvalues': np.random.exponential(scale=1, size=1000)
         })
 
         # Call the generate_plot function
@@ -26,6 +28,7 @@ class MyTestCase(unittest.TestCase):
 
         # Assert that no errors occurred during the plot generation
         self.assertTrue(True)
+
     def test_generate_qqplot_polynomial(self):
         np.random.seed(1)
 
@@ -33,7 +36,7 @@ class MyTestCase(unittest.TestCase):
         x = np.linspace(1, 10, 100)
         coefficients = [2, 3, -5, 10]
         data = pd.DataFrame({
-            'pvalues': np.polyval(coefficients,x)
+            'pvalues': np.polyval(coefficients, x)
         })
 
         # Call the generate_plot function
@@ -42,5 +45,67 @@ class MyTestCase(unittest.TestCase):
         # Assert that no errors occurred during the plot generation
         self.assertTrue(True)
 
+
+class TestReadGeno(unittest.TestCase):
+    def test_read_vcf_single_id(self):
+        out = read_geno("./testfiles/vcf/single_id.vcf")
+        out_dict = out.to_dict()
+        self.assertIsInstance(out['POS'][0], numpy.int32)
+        self.assertIsInstance(out['A'][0], numpy.int8)
+        self.assertEqual(out_dict,
+                         {'CHROM': {0: 1},
+                          'POS': {0: 2},
+                          'ID': {0: 'rs1'},
+                          'REF': {0: 'A'},
+                          'ALT': {0: '.'},
+                          'QUAL': {0: '.'},
+                          'FILTER': {0: '.'},
+                          'INFO': {0: '.'},
+                          'FORMAT': {0: 'GT'},
+                          'A': {0: 1}}
+                         )
+
+    def test_read_vcf_multiple_id(self):
+        out = read_geno("./testfiles/vcf/multi_id.vcf")
+        out_dict = out.to_dict()
+        self.assertIsInstance(out['POS'][0], numpy.int32)
+        self.assertIsInstance(out['A'][0], numpy.int8)
+        self.assertIsInstance(out['B'][0], numpy.int8)
+        self.assertIsInstance(out['C'][0], numpy.int8)
+        self.assertEqual(out_dict,
+                         {'CHROM': {0: 1},
+                          'POS': {0: 2},
+                          'ID': {0: 'rs1'},
+                          'REF': {0: 'A'},
+                          'ALT': {0: '.'},
+                          'QUAL': {0: '.'},
+                          'FILTER': {0: '.'},
+                          'INFO': {0: '.'},
+                          'FORMAT': {0: 'GT'},
+                          'A': {0: 1},
+                          'B': {0: 2},
+                          'C': {0: 3}}
+                         )
+    def test_read_vcf_multi_line(self):
+        out = read_geno("./testfiles/vcf/multi_line.vcf")
+        out_dict = out.to_dict()
+        self.assertIsInstance(out['POS'][0], numpy.int32)
+        self.assertIsInstance(out['A'][0], numpy.int8)
+        self.assertIsInstance(out['B'][0], numpy.int8)
+        self.assertIsInstance(out['C'][0], numpy.int8)
+        self.assertEqual(out_dict,
+                         {'CHROM': {0: 1, 1: 2, 2: 3},
+                          'POS': {0: 1, 1: 2, 2: 3},
+                          'ID': {0: 'rs1', 1: 'rs2', 2: 'rs3'},
+                          'REF': {0: 'C', 1: 'G', 2: 'G'},
+                          'ALT': {0: '.', 1: '.', 2: '.'},
+                          'QUAL': {0: '.', 1: '.', 2: '.'},
+                          'FILTER': {0: '.', 1: '.', 2: '.'},
+                          'INFO': {0: '.', 1: '.', 2: '.'},
+                          'FORMAT': {0: 'GT', 1: 'GT', 2: 'GT'},
+                          'A': {0: 1, 1:3, 2: 2},
+                          'B': {0: 1, 1:3, 2: 2},
+                          'C': {0: 1, 1:3, 2: 2}}
+                         )
 if __name__ == '__main__':
     unittest.main()
